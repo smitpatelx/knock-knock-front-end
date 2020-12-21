cd0014a02f40508f2f2bf42082ee3b78a308a582
Smit Patel <smitbpatel1999@gmail.com>
2020-12-21 02:22:25 -0600

    Delete all, Get random joke, List commands

--- src/js/api.js
+++ src/js/api.js
@@ -10,6 +10,61 @@ const API = {
                 Reject("No joke found")
             }
         })
+    },
+    getRandomJoke() {
+        return new Promise(async (Resolve, Reject)=>{
+            const jokeData = await fetch("http://localhost:8080/get-random/", {
+                method: "GET"
+            });
+            if(jokeData){
+                const jsonRes = await jokeData.json()
+                
+                if(jsonRes.errorCode==="ErrorJokeEmpty") {
+                    Reject(jsonRes.errorMessage)
+                } else {
+                    Resolve(jsonRes)
+                }
+            } else {
+                Reject("No joke found")
+            }
+        })
+    },
+    saveJoke(data){
+        return new Promise(async (Resolve, Reject)=>{
+            const jokeData = await fetch("http://localhost:8080/jokes/", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+            if(jokeData){
+                Resolve(jokeData.json())
+            } else {
+                Reject("Error saving your Joke! Please try again.")
+            }
+        })
+    },
+    deleteAllJokes(){
+        return new Promise(async (Resolve, Reject)=>{
+            const jokeData = await fetch("http://localhost:8080/clear-all/", {
+                method: "DELETE",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
+            if(jokeData){
+                const jsonRes = await jokeData.json()
+                
+                if(jsonRes.errorCode==="ErrorDelJoke") {
+                    Reject(jsonRes.errorMessage)
+                } else {
+                    Resolve(jsonRes)
+                }
+            } else {
+                Reject("Error deleting all jokes!")
+            }
+        })
     }
 }
 
--- src/js/chat.js
+++ src/js/chat.js
@@ -28,52 +28,54 @@ class Chat {
     mountChat(chatInstance){
         const chatContainer = document.querySelector(".chat-bubble-container");
         chatContainer.appendChild(chatInstance)
+        // Scroll to bottom of the chat container
+        chatContainer.scrollTop = chatContainer.scrollHeight;
     }
     async resopnd(){
         const tellMeJoke = new RegExp(/\btell\b/gm);
         const startSequence = new RegExp(/\bknock knock\b/gm);
+        const showAllCommands = new RegExp(/^\$show-all-cmd\b/gm);
+        const deleteAllJoke = new RegExp(/^\$delete-all\b/gm);
         const command = this.unfiltered.toString().toLowerCase();
+
         if (tellMeJoke.exec(command) && this.queStart===false) {
             // Fetch new Jokes
             try {
-                const data = await API.getJoke();
-                console.log(data)
+                const data = await API.getRandomJoke();
+                this.chatContent = data.joke_content;
+                const newChat = this.renderChatBubble("left");
+                this.mountChat(newChat);
             } catch(err) {
                 this.chatContent = "Sorry we couldn't find any jokes! But you can add one. Type <b>\"Knock Kncok\"</b>";
                 const newChat = this.renderChatBubble("left");
                 this.mountChat(newChat);
             }
+        } else if (showAllCommands.exec(command) && this.queStart===false) {
+            // Show list of all commands
+            this.chatContent = "<b>tell me a joke</b> : Tells you a random joke from our API.<br/><b>knock knock</b> : Ask your input to create a new joke.<br/><b>$clear-all</b> : Clear all chat from index DB.<br/><b>$delete-all</b> : Delete all jokes from our API.";
+            const newChat = this.renderChatBubble("left");
+            this.mountChat(newChat);
+        } else if (deleteAllJoke.exec(command) && this.queStart===false) {
+            // Deleting all jokes
+            try {
+                const data = await API.deleteAllJokes();
+                this.chatContent = "Successfully deleted all jokes!";
+                const newChat = this.renderChatBubble("left");
+                this.mountChat(newChat);
+            } catch(err) {
+                this.chatContent = err;
+                const newChat = this.renderChatBubble("left");
+                this.mountChat(newChat);
+            }
         } else if (startSequence.exec(command) && this.queStart===false) {
-            //Ask for a new joke
+            // Ask for a new joke
             this.queStart=true
             console.log("Start new joke sequence")
         }
 
-        //Joke Sequence
+        // Joke Sequence
         if(this.queStart===true && this.currentSequence<=(this.questions.length)){
-            this.answers.push(this.chatContent);
-            if(this.currentSequence===1){
-                this.chatContent = this.unfiltered+this.questions[this.currentSequence];
-            } else {
-                this.chatContent = this.questions[this.currentSequence];
-            }
-            this.formattedQuestion.push(this.chatContent)
-            const newChat = this.renderChatBubble("left");
-            this.mountChat(newChat);
-
-            this.currentSequence++
-            if (this.currentSequence==(this.questions.length)){
-                let text = '';
-                this.formattedQuestion.map((q, index)=>{
-                    text+="<b>User    : </b>"+this.answers[index]+"<br/>"
-                    text+="<b>JokeBot : </b>"+q+"<br/>"
-                })
-                this.chatContent=text
-                const newChat = this.renderChatBubble("left");
-                this.mountChat(newChat);
-                console.log(text);
-                this.resetSequence();
-            }
+            this.initializeJokeSeq();
         }
     }
     sanitize(characters){
@@ -86,9 +88,45 @@ class Chat {
             .replace(/>/g, "&gt;");
     }
     resetSequence(){
+        this.formattedQuestion=[]
         this.queStart=false;
         this.answers=[]
         this.currentSequence=0
+    }
+    async initializeJokeSeq(){
+        this.answers.push(this.chatContent);
+        if(this.currentSequence===1){
+            this.chatContent = this.unfiltered+this.questions[this.currentSequence];
+        } else {
+            this.chatContent = this.questions[this.currentSequence];
+        }
+        this.formattedQuestion.push(this.chatContent)
+        const newChat = this.renderChatBubble("left");
+        this.mountChat(newChat);
+
+        this.currentSequence++
+        if (this.currentSequence==(this.questions.length)){
+            let text = '';
+            this.formattedQuestion.map((q, index)=>{
+                text+="<b>User    : </b>"+this.answers[index]+"<br/>"
+                text+="<b>JokeBot : </b>"+q+"<br/>"
+            })
+            this.chatContent=text
+            const newChat = this.renderChatBubble("left");
+            this.mountChat(newChat);
+
+            const jokeFinalData = {joke_content: text, pub_date: new Date().toISOString()}
+
+            try {
+                await API.saveJoke(jokeFinalData);
+            } catch(err) {
+                this.chatContent = err;
+                const newChat = this.renderChatBubble("left");
+                this.mountChat(newChat);
+            }
+
+            this.resetSequence();
+        }
     }
 }
 
